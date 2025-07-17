dbf39ef2f32bf86a921b12b20e6adc73b267e2f1
atharvamhaske <atharvamhaske76@gmail.com>
2025-07-18 01:26:34 +0530

    created pages and added all components

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,1 +1,16 @@
-export default function Signup() {}
+import { shadowsIntoLight, bangers, monaSans } from "@/app/fonts/fonts";
+
+export default function Signup() {
+    return (
+        <div className="flex flex-col items-center min-h-screen py-8">
+            <div className="flex flex-col items-center mb-12">
+                <h1 className={`${shadowsIntoLight.className} md:text-5xl text-3xl `}>Welcome to <span className={`${bangers.className} md:text-5xl text-3xl text-emerald-500/80 `}>Lumina</span></h1>
+                <p className={`${monaSans.className} md:text-xl text-xs text-gray-200  `}>Please sign up to continue</p>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+                <h2 className={`${shadowsIntoLight.className} md:text-3xl text-2xl `}>Create an account</h2>
+                <p className={`${monaSans.className} md:text-lg text-xs text-gray-200  `}>Please fill in the following details to create an account</p>
+            </div>
+        </div>
+    )
+}
--- app/fonts/fonts.ts
+++ app/fonts/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Edu_QLD_Hand, Mona_Sans } from "next/font/google";
+import { Inter, Edu_QLD_Hand, Mona_Sans, Shadows_Into_Light, Bangers } from "next/font/google";
 
 export const inter = Inter({
     subsets: ["latin"],
@@ -18,5 +18,19 @@ export const monaSans = Mona_Sans({
     subsets: ["latin"],
     variable: "--font-mona-sans",
     weight: ["400", "500", "600", "700"],
+    display: "swap",
+});
+
+export const shadowsIntoLight = Shadows_Into_Light({
+    subsets: ["latin"],
+    variable: "--font-shadows-into-light",
+    weight: ["400"],
+    display: "swap",
+});
+
+export const bangers = Bangers({
+    subsets: ["latin"],
+    variable: "--font-bangers",
+    weight: ["400"],
     display: "swap",
 });
